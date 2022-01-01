0478065f867a95d872b5bb40618ece481c7ec0ea
Viacheslav Miroshko <ccctux@gmail.com>
2022-01-01 18:48:18 +0200

    fixed bootstrap styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/App/';
 import 'normalize.css';
-import './styles.module.scss';
+import './styles.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
